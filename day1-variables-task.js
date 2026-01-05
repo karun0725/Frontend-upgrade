8105bfefda8d357d0a6477f33eb793aee906d789
karun0725 <76605642+karun0725@users.noreply.github.com>
2026-01-05 09:08:52 +0530

    JS variables

---
+++ day1-variables-task.js
@@ -0,0 +1,23 @@
+let score = 0;
+score = score + 10;
+console.log("Score:", score);
+
+const appName = "frontend upgrade";
+console.log("App Name:", appName);
+
+const user = {
+  name: "amor"
+  role: "UI dev"
+};
+user.role = "frontend dev";
+console.log("user:", user);
+
+if(true) {
+  let message = "Insie block";
+  console.log(message);
+}
+
+if(true) {
+  var test = "var scope";
+}
+console.log(test);
